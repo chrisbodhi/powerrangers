244d388b562dc59b0e44db1b18f3a223c6746d9f
chrisbodhi <cboette@gmail.com>
2013-11-19 07:33:45 -0600

    Add methods for the Person class.

--- powerranger.rb
+++ powerranger.rb
@@ -1,7 +1,26 @@
 class Person
+
+  def initialize(name)
+    @name = name
+    @caffeine_level = 0
+  end
+
+  def run
+    puts "Beat feet!"
+  end
+
+  def scream
+    puts "AAAAAAAAAAHHHHHHH *gasp* HHHHHHHHH!"
+  end
+
+  def drink_coffee
+    puts "Seasonal pumpkin spice beverage ingested. +5"
+    @caffeine_level += 5
+  end
+
 end
 
-class PowerRanger
+class PowerRanger < Person
 end
 
 class EvilNinja
