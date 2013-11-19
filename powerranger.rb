ce5d195df10def3ead7d459af338677d0e65cc82
chrisbodhi <cboette@gmail.com>
2013-11-19 12:20:16 -0600

    Add methods for Power Ranger class. Add initialization and methods for Evil Ninja class.

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,5 @@
 class Person
+attr_accessor :caffeine_level
 
   # Initialize the instance. Name is the only parameter taken. Caffeine level is
   # set to a default of zero.
@@ -25,18 +26,51 @@ class Person
 
 end
 
+# Need to add comments for methods
 class PowerRanger < Person
 
-  def initialize
+  def initialize(strength, color)
     super
+    @strength = strength
+    @color = color
+  end
+
+  def punch(person)
+    if @strength > 5
+      person.scream
+      person.run
+      @caffeine_level -= 1
+    end
+  end
+
+  def rest
+  end
+
+  def use_megazord(person)
+    @strength = 5000
+    punch(person)
   end
 
 end
 
 class EvilNinja < Person
 
-  def initialize
+  def initialize(strength, evilness)
     super
+    @strength = strength
+    @evilness = evilness
+  end
+
+  def punch(person)
+    if @strength > 5
+      person.scream
+      person.run
+      @caffeine_level -= 1
+    end
+  end
+
+  def cause_mayhem(person)
+    @caffeine_level = 0
   end
 
 end
