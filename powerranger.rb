33fa86bd6868311e67bba15e33eec2ac01c81679
chrisbodhi <cboette@gmail.com>
2013-11-19 07:37:14 -0600

    Add comments to methods for Person class. Start framework for Power Ranger and Evil Ninja classes.

--- powerranger.rb
+++ powerranger.rb
@@ -1,18 +1,23 @@
 class Person
 
+  # Initialize the instance. Name is the only parameter taken. Caffeine level is
+  # set to a default of zero.
   def initialize(name)
     @name = name
     @caffeine_level = 0
   end
 
+  # Puts a statement related to running.
   def run
     puts "Beat feet!"
   end
 
+  # Puts a statement about screaming.
   def scream
     puts "AAAAAAAAAAHHHHHHH *gasp* HHHHHHHHH!"
   end
 
+  # Puts a statement about coffee, increments the caffeine level by 5.
   def drink_coffee
     puts "Seasonal pumpkin spice beverage ingested. +5"
     @caffeine_level += 5
@@ -21,7 +26,17 @@ class Person
 end
 
 class PowerRanger < Person
+
+  def initialize
+    super
+  end
+
 end
 
-class EvilNinja
+class EvilNinja < Person
+
+  def initialize
+    super
+  end
+
 end
